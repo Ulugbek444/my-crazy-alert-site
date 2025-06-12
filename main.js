df9c4936a0b2f9f2179d8eca5bb91de8d504d690
Ulugbek444 <152319443+Ulugbek444@users.noreply.github.com>
2025-06-13 01:21:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ alert("Нажми окей бля!");
 alert("Так сложно нажать окей?");
 alert("Слушай ты походу там совсем уже");
 alert("Брооооооо! нажмиииииии окейййййй");
+
+const content = document.getElementById("content");
+
+const h1_1 = document.createElement("h1");
+h1_1.textContent = "ХА ЗАСКАМИЛИ МАМОНТА";
+
+const h1_2 = document.createElement("h1");
+h1_2.textContent = "ЛОООХХ!";
+
+content.appendChild(h1_1);
+content.appendChild(h1_2);
